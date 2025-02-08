6deca0066addcb322f2e39fdbd47558d0b416ea0
Bala Logesh S <balassmp@gmail.com>
2025-02-08 16:43:26 -0500

    Initial commit from backend

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send(`Hello, I am running on port ${PORT}`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
