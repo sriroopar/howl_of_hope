2b7acf9da26ed3e02a52e7b36115398eda4b3cf7
Bala Logesh S <balassmp@gmail.com>
2025-02-08 17:10:17 -0500

    Added mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+import connectDB from './utils/db.js'
 
 const app = express()
 app.use(cors())
 
 const PORT = process.env.PORT || 3000
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send(`Hello, I am running on port ${PORT}`)
