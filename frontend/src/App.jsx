41d2e65a2e7617733205edfc886cbb001070bfb0
Bala Logesh S <balassmp@gmail.com>
2025-02-08 19:21:39 -0500

    Added styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
+import './styles/App.css'
+import './styles/variables.css'
+import './styles/header-footer.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './pages/NavBar'
 import Login from './pages/Login';
 import Register from './pages/Register';
 import UserProfile from './pages/UserProfile';
 import Landing from './pages/Landing';
+import Footer from './pages/Footer';
 
 function App() {
 
@@ -13,12 +16,15 @@ function App() {
       <div className="container">
         <BrowserRouter>
           <NavBar />
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={<UserProfile />} />
-            <Route path='/' exact element={<Landing />} />
-          </Routes>
+          <div className='main wrapper'>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/profile" element={<UserProfile />} />
+              <Route path='/' exact element={<Landing />} />
+            </Routes>
+          </div>
+          <Footer />
         </BrowserRouter>
       </div>
     </>
--- frontend/src/pages/NavBar.jsx
+++ frontend/src/pages/NavBar.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 function NavBar() {
+    const [isAuthenticated, setIsAuthenticated] = useState(true)
+
     return (
         <nav>
-            <Link to="/">Howl Of Hope</Link>
-            <ul>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
-                <Link to="/profile">User Profile</Link>
-            </ul>
-        </nav>
+            <div className="wrapper">
+                <h1><Link to="/">Howl Of Hope</Link></h1>
+                <ul>
+                    {!isAuthenticated ? (
+                        <>
+                            <NavLink to="/login" className={({ isActive }) => isActive ? "active" : ""}>Login</NavLink>
+                            <NavLink to="/register" className={({ isActive }) => isActive ? "active" : ""}>Register</NavLink>
+                        </>
+                    ) : (
+                        <NavLink to="/profile" className={({ isActive }) => isActive ? "active" : ""}>User Profile</NavLink>
+                    )}
+                </ul>
+            </div>
+        </nav >
     )
 }
 
