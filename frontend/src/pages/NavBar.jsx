4a0a9934b450b467727e5bb4724b253e20c8be21
Saikrishna Rajaraman <saikrishnarajaraman96@gmail.com>
2025-02-08 22:06:34 -0500

    Added google authentication

--- frontend/src/pages/NavBar.jsx
+++ frontend/src/pages/NavBar.jsx
@@ -1,11 +1,39 @@
 import { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import { auth, googleProvider } from '../config/firebase';  
+import { signInWithPopup } from 'firebase/auth';
+import { signOut } from 'firebase/auth';
 
 function NavBar() {
     const [isAuthenticated, setIsAuthenticated] = useState(false)
 
+    const signInWithGoogle = async () => {
+        try {
+          await signInWithPopup(auth, googleProvider);
+          alert('Signed in successfully with Google');
+          setIsAuthenticated(prev=>!prev)
+        } catch (error) {
+          console.error('Error signing in with Google', error);
+        }
+      };
+
+    const handleSignOut = async () => {
+        try {
+          await signOut(auth);
+          alert('User signed out successfully');
+          setIsAuthenticated(prev=>!prev)
+        } catch (error) {
+          console.error('Error signing out:', error);
+        }
+      };  
+
     const handleUserSignIn = () => {
-        console.log('Sign in attempt')
+        signInWithGoogle()
+    }
+
+    const handleUserSignOut = () => {
+        console.log("Sign out")
+        handleSignOut()
     }
 
     return (
@@ -18,9 +46,13 @@ function NavBar() {
                             <Link onClick={handleUserSignIn}>Sign in with Google</Link>
                         </>
                     ) : (
+                        <>
                         <NavLink to="/profile" className={({ isActive }) => isActive ? "active" : ""}>User Profile</NavLink>
+                        <Link onClick={handleUserSignOut}>Sign Out</Link>
+                        </> 
                     )}
                 </ul>
+                
             </div>
         </nav >
     )
